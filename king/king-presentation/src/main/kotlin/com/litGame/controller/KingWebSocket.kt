f43179df1c438bb2bb6d2d755786161b25b709a4
Seungkyu-Han <trust1204@gmail.com>
2025-07-16 08:07:27 +0900

    docs: change king game socket swagger docs

--- king/king-presentation/src/main/kotlin/com/litGame/controller/KingWebSocket.kt
+++ king/king-presentation/src/main/kotlin/com/litGame/controller/KingWebSocket.kt
@@ -1,5 +1,6 @@
 package com.litGame.controller
 
+import com.litGame.dto.req.PlayGameDto
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.Parameters
@@ -11,17 +12,18 @@ import org.springframework.web.bind.annotation.*
 @Tag(name = "왕게임 소켓 문서(REST API 사용 X)")
 class KingWebSocket {
 
-    @GetMapping("/sub/{roomId}")
-    @Operation(summary = "게임방 구독", description = "{'message': 'a'}의 형식입니다...")
+    @PostMapping("/sub/{roomId}")
+    @Operation(summary = "게임방 구독", description = "{'message': 'a'}의 형식입니다..."    )
     @Parameters(
-        Parameter(name = "roomId", description = "구독할 방의 번호")
+        Parameter(name = "roomId", description = "구독할 방의 번호"),
+
     )
-    fun subscribe(@PathVariable roomId: String){}
+    fun subscribe(@PathVariable roomId: String, @RequestBody playerDto: PlayGameDto) {}
 
     @PostMapping("/pub/{roomId}")
     @Operation(summary = "게임방 이벤트 전송", description = "{'message': 'a'}의 형식입니다...")
     @Parameters(
         Parameter(name = "roomId", description = "이벤트를 전송할 방의 번호")
     )
-    fun publish(@PathVariable roomId: String){}
+    fun publish(@PathVariable roomId: String, playerDto: PlayGameDto){}
 }
