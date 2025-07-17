abf69067f5418c69f68cfd0d50571f50aa86f5e8
한승규 <98071131+Seungkyu-Han@users.noreply.github.com>
2025-07-17 14:47:19 +0900

    Update WebConfig.kt

--- container/src/main/kotlin/com/litGame/config/WebConfig.kt
+++ container/src/main/kotlin/com/litGame/config/WebConfig.kt
@@ -9,7 +9,7 @@ class WebConfig: WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOriginPatterns("*")
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
