3b17ae297c036cca6f5f26c2a16ec30a80ade7b6
Seungkyu-Han <trust1204@gmail.com>
2025-07-20 00:38:13 +0900

    fix: https cors error

--- container/src/main/kotlin/com/litGame/config/WebConfig.kt
+++ container/src/main/kotlin/com/litGame/config/WebConfig.kt
@@ -1,15 +1,19 @@
 package com.litGame.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig: WebMvcConfigurer {
+class WebConfig(
+    @Value("\${spring.server_url}")
+    private val serverUrl: String
+): WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")
+            .allowedOrigins(serverUrl)
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
