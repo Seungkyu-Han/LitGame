921fc5bc5c5318f78a19fe70e8db352248af67c8
Seungkyu-Han <trust1204@gmail.com>
2025-07-20 13:16:11 +0900

    fix: 404 not found error

--- container/src/main/kotlin/com/litGame/exception/GlobalExceptionHandler.kt
+++ container/src/main/kotlin/com/litGame/exception/GlobalExceptionHandler.kt
@@ -5,12 +5,18 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.servlet.resource.NoResourceFoundException
 import java.time.LocalDateTime
 
 @ControllerAdvice
 class GlobalExceptionHandler(
     private val errorReporter: ErrorReporter
 ) {
+    @ExceptionHandler(NoResourceFoundException::class)
+    fun handleNoResourceFoundException(e: NoResourceFoundException): ResponseEntity<Void> {
+        return ResponseEntity.notFound().build()
+    }
+
 
     @ExceptionHandler(Exception::class)
     fun handleException(e: Exception): ResponseEntity<ErrorResponse> {
